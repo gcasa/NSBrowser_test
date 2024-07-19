6195beb046527b1d96def931ce839099e4e33bd3
Gregory Casamento <greg.casamento@gmail.com>
2024-07-19 11:16:37 -0400

    Initial Commit

---
+++ NSBrowser_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSBrowser_test
+//
+//  Created by Gregory John Casamento on 7/19/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSBrowser_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSBrowser_test
+//
+//  Created by Gregory John Casamento on 7/19/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
